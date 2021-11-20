0102b848da9f24c7e76f07b7287485d4d86e4333
Bruno Conterato <brunoconterato@gmail.com>
2021-11-20 17:14:11 -0300

    Updating Utils to work with multi-agent

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import gym
+from ray.rllib import MultiAgentEnv
+import soccer_twos
+
+
+class RLLibWrapper(gym.core.Wrapper, MultiAgentEnv):
+    """
+    A RLLib wrapper so our env can inherit from MultiAgentEnv.
+    """
+
+    pass
+
+
+def create_rllib_env(env_config: dict = {}):
+    """
+    Creates a RLLib environment and prepares it to be instantiated by Ray workers.
+    Args:
+        env_config: configuration for the environment.
+            You may specify the following keys:
+            - variation: one of soccer_twos.EnvType. Defaults to EnvType.multiagent_player.
+            - opponent_policy: a Callable for your agent to train against. Defaults to a random policy.
+    """
+    if hasattr(env_config, "worker_index"):
+        env_config["worker_id"] = (
+            env_config.worker_index * env_config.get("num_envs_per_worker", 1)
+            + env_config.vector_index
+        )
+    env = soccer_twos.make(**env_config)
+    if "multiagent" in env_config and not env_config["multiagent"]:
+        # is multiagent by default, is only disabled if explicitly set to False
+        return env
+    return RLLibWrapper(env)
