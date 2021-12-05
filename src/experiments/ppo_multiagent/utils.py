a3c20436800e1ee725416956c9a593e51304f4a5
Bruno Conterato <brunoconterato@gmail.com>
2021-12-05 17:01:17 -0300

    Utility for getting trainer and policy

--- src/experiments/ppo_multiagent/utils.py
+++ src/experiments/ppo_multiagent/utils.py
@@ -5,6 +5,15 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from wrapper import CustomRewardWrapper
 
+# get_agent_and_policy imports
+import os
+import pickle
+import ray
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.env import BaseEnv
+from ray.tune.registry import get_trainable_cls
+
 class RLLibWrapper(gym.core.Wrapper, MultiAgentEnv):
     """
     A RLLib wrapper so our env can inherit from MultiAgentEnv.
@@ -37,3 +46,55 @@ def create_rllib_env(env_config: dict = {}):
 def create_custom_env(env_config: dict = {}):
     env = create_rllib_env(env_config)
     return CustomRewardWrapper(env)
+
+
+def get_agent_and_policy(algorithm, checkpoint_path, policy_name):
+    ray.init(ignore_reinit_error=True)
+    config_path = ""
+    if checkpoint_path:
+        config_dir = os.path.dirname(checkpoint_path)
+        config_path = os.path.join(config_dir, "params.pkl")
+        # Try parent directory.
+        if not os.path.exists(config_path):
+            config_path = os.path.join(config_dir, "../params.pkl")
+
+    # Load the config from pickled.
+    if os.path.exists(config_path):
+        with open(config_path, "rb") as f:
+            cfg = pickle.load(f)
+    else:
+        # If no config in given checkpoint -> Error.
+        raise ValueError(
+            "Could not find params.pkl in either the checkpoint dir or "
+            "its parent directory!"
+        )
+
+    # no need for parallelism on evaluation
+    cfg["num_workers"] = 0
+    cfg["num_gpus"] = 0
+    cfg['evaluation_interval'] = 0
+
+    # create a dummy env since it's required but we only care about the policy
+    tune.registry.register_env("DummyEnv", lambda *_: BaseEnv())
+    cfg["env"] = "DummyEnv"
+
+    # create the Trainer from config
+    # cls = get_trainable_cls(algorithm) if isinstance(algorithm, str) else algorithm
+    trainer = PPOTrainer(config=cfg, env=cfg["env"])
+    # agent = cls(env=cfg["env"], config=cfg)
+    # load state from checkpoint
+    trainer.restore(checkpoint_path)
+    # get policy for evaluation
+    policy = trainer.get_policy(policy_name)
+
+    return trainer, policy
+
+
+# baseline_trainer, baseline_policy = get_agent_and_policy("PPO",
+#                                                      "/home/bruno/Workspace/soccer-tows-player/src/ray_results/PPO_selfplay_twos/PPO_Soccer_f475e_00000_0_2021-09-19_15-54-02/checkpoint_002449/checkpoint-2449",
+#                                                      "default",
+#                                                      )
+
+# bajai_trainer, bajai_policy = get_agent_and_policy("PPO",
+#                                                "/home/bruno/Workspace/soccer-tows-player/src/ray_results/PPO_multiagent_player_custom_rewards/PPO_Soccer_491c2_00000_0_2021-11-30_01-16-26/checkpoint_002617/checkpoint-2617",
+#                                                "default")
